39bf17aa5310d623ab784166986f33affca70b29
Nishant Kumar <82407925+nk728@users.noreply.github.com>
2022-01-12 23:42:06 +0530

    Part 2:Solution

--- -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/RestaurantService.java
+++ -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/RestaurantService.java
@@ -18,7 +18,7 @@ public class RestaurantService {
 
 
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
-        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        Restaurant newRestaurant = new Restaurant(name, location,openingTime, closingTime);
         restaurants.add(newRestaurant);
         return newRestaurant;
     }
