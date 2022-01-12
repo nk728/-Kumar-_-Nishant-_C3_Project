f8a2759288c0e61a96d705cde151bee9dfe43bc4
Nishant Kumar <82407925+nk728@users.noreply.github.com>
2022-01-12 23:43:08 +0530

    Part 2:Solution

--- -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/restaurantNotFoundException.java
+++ -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/restaurantNotFoundException.java
@@ -2,6 +2,6 @@ package com.RestaurantFinder;
 
 public class restaurantNotFoundException extends Throwable {
     public restaurantNotFoundException(String restaurantName) {
-        super(restaurantName);
+         super(restaurantName);
     }
 }
