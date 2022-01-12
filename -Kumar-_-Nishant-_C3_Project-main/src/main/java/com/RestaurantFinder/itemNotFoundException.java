e4e454b7fbacfda98893c9792147751cd458374a
Nishant Kumar <82407925+nk728@users.noreply.github.com>
2022-01-12 23:42:44 +0530

    Part 2:Solution

--- -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/itemNotFoundException.java
+++ -Kumar-_-Nishant-_C3_Project-main/src/main/java/com/RestaurantFinder/itemNotFoundException.java
@@ -2,6 +2,6 @@ package com.RestaurantFinder;
 
 public class itemNotFoundException extends Throwable {
     public itemNotFoundException(String itemName) {
-        super(itemName);
+         super(itemName);
     }
 }
